1c7316ac5f05cb80fbfddfdaa409903e4908508f
futantan <luckytantanfu@gmail.com>
2016-02-12 21:18:43 +0800

    add category for UIVeiw

---
+++ UIView/UIView+FTFrame.h
@@ -0,0 +1,17 @@
+//
+//  UIView+FTFrame.h
+//
+//  Created by luckytantanfu on 2/12/16.
+//  Copyright © 2016 futantan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (FTFrame)
+
+@property (nonatomic, assign) CGFloat x;
+@property (nonatomic, assign) CGFloat y;
+@property (nonatomic, assign) CGFloat width;
+@property (nonatomic, assign) CGFloat height;
+
+@end
